2c021fb09672d58301047138846c06d0184ffa8f
Elmahdi ELaazmi <elaazmielmahdi@gmail.com>
2021-01-15 19:18:48 +0100

    Add files via upload

--- libasm/main.c
+++ libasm/main.c
@@ -1,13 +1,19 @@
 #include "libasm.h"
 #include <stdio.h>
-
+#include <string.h>
+ 
 int main(int argc, char **argv)
 {
     if (argc >= 2)
     {
+        /*ft_strlen*/
         //printf("ft_strlen(argv[1]) = %zu\n",ft_strlen(argv[1]));
-        char *s;
+        
+        /*strcpy*/
+        char s[100];
         printf("ft_strcpy(s , d) = |%s|\n", ft_strcpy(s,argv[1]));
+
+
 
     }
     else
