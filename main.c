010a5bc5ea73583fe43555bda68e0a64b1927629
Elmahdi ELaazmi <elaazmielmahdi@gmail.com>
2021-04-11 16:19:58 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -4,56 +4,59 @@
 #include <stdlib.h>
 #include  <fcntl.h>
 
-int	main(int argc, char **argv)
+void	test_read(void)
 {
 	int		o;
+	char	buff[6];
+	char	buff2[6];
+
+	o = ft_read(0, buff, 5);
+	buff[o] = '\0';
+	o = read(0, buff2, 5);
+	buff2[o] = '\0';
+	printf("ft_read => return = (%d) || buff = |%s|\n", o, buff);
+	printf("   read => return = (%d) || buff = |%s|\n", o, buff2);
+	o = ft_write(1, "LLL", 3);
+	printf("\n|^| return = |%d| ft_write()\n\n", o);
+	o = write(1, "LLL", 3);
+	printf("\n|^| return = |%d|    write()\n", o);
+}
+
+void	test_strdup(char *str)
+{
 	char	*s;
 	char	*r;
 
+	printf("ft_strdup(argv[1]) = |%s|\n", (r = ft_strdup(str)));
+	printf("   strdup(argv[1]) = |%s|\n\n", (s = strdup(str)));
+	free(s);
+	free(r);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*s;
+	char	*r;
+	int		d;
+
 	if (argc >= 2)
 	{
-		printf("ft_strlen(argv[1]) = %zu\n",ft_strlen(argv[1]));
-		printf("   strlen(argv[1]) = %zu\n\n",strlen(argv[1]));
-		
+		printf("ft_strlen(argv[1]) = %zu\n", ft_strlen(argv[1]));
+		printf("   strlen(argv[1]) = %zu\n\n", strlen(argv[1]));
 		s = malloc(strlen(argv[1] + 1));
-		printf("ft_strcpy(s , d) = |%s|\n", ft_strcpy(s,argv[1]));
-		printf("   strcpy(s , d) = |%s|\n\n", strcpy(s,argv[1]));
-
+		printf("ft_strcpy(s , d) = |%s|\n", ft_strcpy(s, argv[1]));
+		printf("   strcpy(s , d) = |%s|\n\n", strcpy(s, argv[1]));
 		if (argv[2])
 		{
-			printf("ft_strcmp(argv[1], argv[2]) = |%d|\n" ,ft_strcmp(argv[1],argv[2]));
-			printf("   strcmp(argv[1], argv[2]) = |%d|\n\n" ,  strcmp(argv[1],argv[2]));
+			printf("ft_strcmp(argv[1], argv[2]) = |%d|\n", \
+			ft_strcmp(argv[1], argv[2]));
+			printf("   strcmp(argv[1], argv[2]) = |%d|\n\n", \
+			strcmp(argv[1], argv[2]));
 		}
-
-		printf("ft_strdup(argv[1]) = |%s|\n",  (r = ft_strdup(argv[1])));
-		printf("   strdup(argv[1]) = |%s|\n\n",  (s =    strdup(argv[1])));
-		free(s);
-		free(r);
-    
-		char buff[6];
-		int d = ft_read(0, buff, 5);
-		buff[d] = '\0';
-
-		char buff2[6];
-		d = read(0, buff2, 5);
-		buff2[d] = '\0';
-		printf("ft_read => return = (%d) || buff = |%s|\n", d, buff);
-		printf("   read => return = (%d) || buff = |%s|\n", d, buff2);
-
-
-// fd = open(argv[1], O_RDONLY) ;
-// char yy[11];
-// d = read(fd, yy, 10);
-// yy[d] = '\0';
-// printf("fd = %d | (%d) =    read(fd, buff, 10)\n|%s|\n",fd , d, yy);
-
-//6 FT_WRITE
-        o = ft_write(1, "LLL", 3);
-        printf("\n|^| return = |%d| ft_write()\n\n", o);
-        o = write(1, "LLL" , 3);
-        printf("\n|^| return = |%d|    write()\n", o);
-        }
-    else
-        printf("no input\n");
-    return (0);
+		test_strdup(argv[1]);
+		test_read();
+	}
+	else
+		printf("no input\n");
+	return (0);
 }
